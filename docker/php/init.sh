6ac695eb9e79794d8ff410e0f1cb0b2053d9d3cc
Thiago Tognoli <thiagotognoli@gmail.com>
2020-07-17 22:40:46 -0300

    docker

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -3,7 +3,7 @@ if [ ! -e ../static/.inited ]
 then 
     cp -R public ../static/.
     mv storage ../static/.
-    ln -sf ../static/storage storage
+    ln -sf storage ../static/storage
     touch ../static/.inited
 fi
 php-fpm
