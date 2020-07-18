f5c2d9262e7186168b0baa8148fb17a7cf4934c7
Thiago Tognoli <thiagotognoli@gmail.com>
2020-07-18 00:16:01 -0300

    docker

--- docker/dnsmasq/init.sh
+++ docker/dnsmasq/init.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+touch /var/www/html/storage/app/dnsmasq/addn-hosts.conf
+touch /var/www/html/storage/app/dnsmasq/hosts
+touch /var/www/html/storage/app/dnsmasq/records.conf
+touch /var/www/html/storage/app/dnsmasq/resolv
+touch /var/www/html/storage/app/dnsmasq/resolv.conf
+
+
 while sleep 1; do
-    ls /etc/dnsmasq.d/. | entr -r dnsmasq
+    ls /var/www/html/storage/app/dnsmasq/. | entr -r dnsmasq
 done
